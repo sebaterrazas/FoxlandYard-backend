907226f2da86a3690e0b1ef3b9fd0ad2b6bd3e33
SEBASTIAN TERRAZAS <sebaterrazas@uc.cl>
2023-07-04 19:26:51 -0400

    scripts2

--- start.js
+++ start.js
@@ -5,12 +5,12 @@ async function deploy() {
     const { stdout: output1} = await exec("yarn install");
     console.log("Installing dependencies...");
     console.log(output1);
-    
+
     const { stdout: output2} = await exec("yarn db:migrate");
     console.log("Migrating database...");
     console.log(output2);
 
-    const { stdout: output3} = await exec("yarn db: seed");
+    const { stdout: output3} = await exec("yarn db:seed");
     console.log("Seeding database...");
     console.log(output3);
 }
